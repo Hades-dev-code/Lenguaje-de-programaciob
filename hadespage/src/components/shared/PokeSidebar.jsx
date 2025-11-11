929d11a9a6924906fcd485dbc45120e337fdf48b
The_Hades <ortizeduardo.27072004@gmail.com>
2025-11-11 17:56:26 -0400

    Change avatar image in PokeSidebar component
    Updated avatar image source from 'avatar.png' to 'profile-pic.png'.

--- hadespage/src/components/shared/PokeSidebar.jsx
+++ hadespage/src/components/shared/PokeSidebar.jsx
@@ -66,7 +66,7 @@ function PokeSidebar({ setPokemonId }) {
         className="w-14 h-14 rounded-full overflow-hidden border-2 border-indigo-500 mb-6 cursor-pointer"
       >
         <img
-          src="../../assets/avatar.png"
+          src="../../assets/profile-pic.png"
           alt="Avatar"
           className="w-full h-full object-cover"
         />
@@ -114,4 +114,5 @@ function PokeSidebar({ setPokemonId }) {
 }
 
 export default PokeSidebar;
+
 
